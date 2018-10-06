e5067ded26390d6cde5ef72fee8b814d6022c353
Mays3200 <35819141+Mays3200@users.noreply.github.com>
2018-10-06 16:51:26 -0400

    updated

--- doc/server.r
+++ doc/server.r
@@ -1,74 +1,345 @@
-library(ggmap)
-library(ggplot2)
+library(shiny)
+library(leaflet)
+library(data.table)
+library(devtools)
+library(MASS)
 library(dplyr)
+library(tigris)
+library(sp)
+library(maptools)
+library(broom)
+library(httr)
+library(rgdal)
+library(RColorBrewer)
+library(XML)
 library(DT)
+library(dplyr)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(ggmap)
+getwd()
+load("../data/avg_price_zip.RData")
+load("../data/subdat.RData")
+load("../data/housing.RData")  
+source("../lib/showPopupHover.R")
+source("../lib/ZillowApi.R")
 
-load("C:/Users/maysh/Documents/avg_price_zip.RData")
-load("C:/Users/maysh/Documents/subdat.RData")
+color <- list(color1 = c('#F2D7D5','#D98880', '#CD6155', '#C0392B', '#922B21','#641E16'),
+              color2 = c('#e6f5ff','#abdcff', '#70c4ff', '#0087e6', '#005998','#00365d','#1B4F72'),
+              color3 = c("#F7FCF5","#74C476", "#005A32"))
 
 
-shinyServer(function(input, output,session){
-     #################################################################
-     ##### Panel 1 : I cook food  #####################################
-     #################################################################
-     output$map <- renderLeaflet({
-       leaflet()%>%
-         setView(lng = -73.98928, lat = 40.75042, zoom = 13)%>%
-         addProviderTiles("OpenStreetMap.HOT")
+getwd()
+shinyServer(function(input, output,session) {
+  
+  #Esri.WorldTopoMap
+  #########main map######
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles('Esri.WorldTopoMap') %>%
+      setView(lng = -73.971035, lat = 40.775659, zoom = 12) %>%
+      addMarkers(data=housing,
+                 lng=~lng,
+                 lat=~lat,
+                 clusterOptions=markerClusterOptions(),
+                 group="housing_cluster"
+      )
+  })
+  
+  
+  
+  #############Housing#############
+  
+  
+  # filter housing data:
+  
+  housingFilter=reactive({
+    bedroom_filter=housing$bedrooms>input$min_bedrooms 
+    bathroom_filter=housing$bathrooms>input$min_bathrooms
+    price_filter=housing$price>=input$min_price & housing$price<=input$max_price
+    filter=bedroom_filter & bathroom_filter & price_filter
+    return(housing[filter,])
+  })
+  
+  # show data in the map:
+  observe({leafletProxy("map")%>%clearGroup("housing_cluster")%>%
+      addMarkers(data=housingFilter(),
+                 lng=~lng,
+                 lat=~lat,
+                 clusterOptions=markerClusterOptions(),
+                 group="housing_cluster"
+      )
+  })
+  # show current status of icons:
+  showStatus=reactive({
+    if (is.null(input$map_bounds)){
+      return("cloud")
+      
+    }
+    else{
+      if(input$map_zoom<16){
+        return('cloud')
+      }
+      else{
+        return('details')
+      }
+    }
+  })
+  # hide and show clouds 
+  observe({
+    if(showStatus()=="cloud"){
+      
+      leafletProxy("map") %>%showGroup("housing_cluster")%>%clearGroup("new_added")
+    }
+    else{
+      leafletProxy("map") %>%hideGroup("housing_cluster")
+      
+    }
+  })
+  
+  
+  # get the housing data in the bounds
+  marksInBounds <- reactive({
+    if (is.null(input$map_bounds))
+      return(housing[FALSE,])
+    bounds <- input$map_bounds
+    latRng <- range(bounds$north, bounds$south)
+    lngRng <- range(bounds$east, bounds$west)
     
-     })
+    return(
+      subset(housingFilter(),
+             lat>= latRng[1] & lat <= latRng[2] &
+               lng >= lngRng[1] & lng <= lngRng[2])
+    )
+  })
   
-     ## Panel *: heat map###########################################
-     # ----- set uo color pallette https://rstudio.github.io/leaflet/colors.html
-     # Create a continuous palette function
-     pal <- colorNumeric(
-       palette = "Greens",
-       domain = subdat$value
-       )
-     leafletProxy("map",data=subdat)%>%
-       addPolygons(layerId = ~ZIPCODE,
-                   stroke = T, weight=1,
-                   fillOpacity = 0.35,
-                   color = ~pal(value),
-                   highlightOptions = highlightOptions(color='#ff0000', opacity = 0.5, weight = 4, fillOpacity = 0.9,
-                                                       bringToFront = TRUE, sendToBack = TRUE))%>%
-       addLegend(pal = pal, values = ~value, opacity = 1)
+  # show housing details when zoom to one specific level
+  observe({
+    if(showStatus()=="details"){
+      if(nrow(marksInBounds())!=0){
+        leafletProxy("map")%>%clearGroup(group="new_added")%>% 
+          addCircleMarkers(data=marksInBounds(),
+                           lat=~lat,
+                           lng=~lng,
+                           label=~as.character(price),
+                           radius=5,
+                           stroke=FALSE,
+                           fillColor = "green",
+                           fillOpacity=0.7,
+                           group="new_added",
+                           labelOptions = labelOptions(
+                             noHide = T,
+                             offset=c(20,-15),
+                             opacity=0.7,
+                             direction="left",
+                             style=list(
+                               background="green",
+                               color="white"  
+                             )
+                           )
+          )
+      }
+      else{
+        leafletProxy("map")%>%clearGroup(group="new_added")
+      }
+    }
+  })
   
   
-     ## Panel *: click on any area, popup text about this zipcode area's information#########
-     observeEvent(input$map_shape_click, {
-       ## track
-       if(input$click_multi == FALSE) leafletProxy('map') %>%clearGroup("click")
-       click <- input$map_shape_click
-       leafletProxy('map')%>%
-         addMarkers(click$lng, click$lat, group="click", icon=list(iconUrl='icon/leaves.png',iconSize=c(60,60)))
+  # sort housing in current zoom level
+  observe({
+    housing_sort=marksInBounds()
     
-       ##info
-       zip_sel<-as.character(revgeocode(as.numeric(c(click$lng,click$lat)),output="more")$postal_code)
-       zip<-paste("ZIPCODE: ",zip_sel)
-       price_avg<-paste("Average Price: $",avg_price_zip.df[avg_price_zip.df$region==zip_sel,"value"],sep="")
-       
-       leafletProxy("map")%>%
-         setView(click$lng,click$lat,zoom=14,options=list(animate=TRUE))
-       
-       output$zip_text<-renderText({zip})
-       output$avgprice_text<-renderText({price_avg})
-       
-     })
+    if(nrow(housing_sort)!=0){
+      
+      action=apply(housing_sort,1,function(r){
+        addr=r["addr"]
+        lat=r["lat"]
+        lng=r["lng"]
+        paste0("<a class='go-map' href='' data-lat='",lat,"'data-lng='",lng,"'>",addr,'</a>')   
+      }
+      )
+      
+      housing_sort$addr=action
+      output$rank <- renderDataTable(housing_sort[,c("addr","price","bedrooms","bathrooms")],escape=F)
+      
+      
+      
+      
+    }
+    else{
+      
+      output$rank=renderDataTable(housing_sort[,c("addr","price","bedrooms","bathrooms")])
+    }
+    
+  })
   
-    ## Panel *: Return to big view##################################
-     observeEvent(input$click_reset_buttom,{
-       if(input$click_reset_buttom){
-         leafletProxy("map")%>%
-           setView(lng = -73.98928, lat = 40.75042, zoom = 13)%>% 
-           clearPopups()
-       }
-     })
+  # When point in map is hovered, show a popup with housing info
+  observe({
+    
+    event <- input$map_marker_mouseover
+    if (is.null(event))
+      return()
+    if(showStatus()=="details"){
+      isolate({
+        showPopupHover(event$lat, event$lng,housing=housingFilter())
+      })  
+    }
+    
+  })
+  
+  # mouseout the point and cancel popup
+  observe({
+    
+    event <- input$map_marker_mouseout
+    if (is.null(event))
+      return()
+    
+    isolate({
+      leafletProxy("map") %>% clearPopups()
+    })
+  })
+  
+  # click name to go to that point
+  observe({
+    if (is.null(input$goto))
+      return()
+    isolate({
+      map <- leafletProxy("map")
+      
+      
+      
+      lat <- as.numeric(input$goto$lat)
+      lng <- as.numeric(input$goto$lng)
+      
+      map %>% setView(lng = lng, lat = lat, zoom = 16)
+    })
+  })
+  # hover the list to show info
+  observe({
+    if (is.null(input$showPop))
+      return()
+    isolate({
+      remove=as.numeric(input$showPop$remove)
+      map <- leafletProxy("map")
+      
+      if(remove==0){
+        
+        
+        
+        lat <- as.numeric(input$showPop$lat)
+        lng <- as.numeric(input$showPop$lng)
+        showPopupHover(lat, lng,housingFilter())   
+      }
+      else{
+        map %>% clearPopups()
+      }
+      
+      
+    })
+  })
+  
+  #############Search###############
+  observeEvent(input$button1,{
+    url = paste0('http://maps.google.com/maps/api/geocode/xml?address=',input$location,'&sensor=false')
+    doc = xmlTreeParse(url) 
+    root = xmlRoot(doc) 
+    lati = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lat']])) 
+    long = as.numeric(xmlValue(root[['result']][['geometry']][['location']][['lng']]))
+    
+    leafletProxy("map") %>%
+      setView(lng=long, lat=lati,zoom=15)%>%
+      addMarkers(lng=long,lat=lati,layerId = "1",icon=icons(
+        iconUrl = "../lib/icons8-Location-50.png",iconWidth = 25, iconHeight = 25))
+  })
+  #################Clear Choices############
+  observeEvent(input$button2,{
+    proxy<-leafletProxy("map")
+    proxy %>%
+      setView(lng = -73.971035, lat = 40.775659, zoom = 12) %>%
+      removeMarker(layerId="1") %>%
+      addMarkers(data=housing,
+                 lng=~lng,
+                 lat=~lat,
+                 clusterOptions=markerClusterOptions(),
+                 group="housing_cluster")
+    updateTextInput(session, inputId="location", value = "")
+  }
+  
+  )
+  
+  
+  #############Clear button###########
+  observeEvent(input$clear, {
+    leafletProxy('map')%>% setView(lng = -73.971035, lat = 40.775659, zoom = 12)
+    
+  })
+  
+  output$map <- renderLeaflet({
+    leaflet()%>%
+      setView(lng = -73.98928, lat = 40.75042, zoom = 13)%>%
+      addProviderTiles("OpenStreetMap.HOT")
+    
+  })
+  
+  
+  ## Panel *: heat map###########################################
+  # ----- set uo color pallette https://rstudio.github.io/leaflet/colors.html
+  # Create a continuous palette function
+  pal <- colorNumeric(
+    palette = "Greens",
+    domain = subdat$value
+  )
+  
+  output$map1 <- renderLeaflet({
+    leaflet(options = leafletOptions(zoomControl = FALSE))%>%
+      addProviderTiles('Esri.WorldTopoMap') %>%
+      setView(lng = -73.96, lat = 40.75042, zoom = 13)%>%
+      
+      addPolygons(layerId = ~ZIPCODE,data=subdat,
+                  stroke = T, weight=1,
+                  fillOpacity = 0.35,
+                  color = ~pal(value),
+                  highlightOptions = highlightOptions(color='#ff0000', opacity = 0.5, weight = 4, fillOpacity = 0.9,
+                                                      bringToFront = TRUE, sendToBack = TRUE))%>%
+      addLegend(data=subdat,pal = pal, values = ~value, opacity = 0.5)
+    
+  })
+  
+  ## Panel *: click on any area, popup text about this zipcode area's information#########
+  observeEvent(input$map_shape_click, {
+    ## track
+    if(input$click_multi == FALSE) leafletProxy('map1') %>%clearGroup("click")
+    click <- input$map_shape_click
+    leafletProxy('map1')%>%
+      addMarkers(click$lng, click$lat, group="click", icon=list(iconUrl='icon/leaves.png',iconSize=c(60,60)))
+    
+    ##info
+    zip_sel<-as.character(revgeocode(as.numeric(c(click$lng,click$lat)),output="more")$postal_code)
+    zip<-paste("ZIPCODE: ",zip_sel)
+    price_avg<-paste("Average Price: $",avg_price_zip.df[avg_price_zip.df$region==zip_sel,"value"],sep="")
+    
+    leafletProxy("map1")%>%
+      setView(click$lng,click$lat,zoom=14,options=list(animate=TRUE))
+    
+    output$zip_text<-renderText({zip})
+    output$avgprice_text<-renderText({price_avg})
+    
+  })
+  
+  
+  ## Panel *: Return to big view##################################
+  observeEvent(input$click_reset_buttom,{
+    if(input$click_reset_buttom){
+      leafletProxy("map1")%>%
+        setView(lng = -73.96, lat = 40.75042, zoom = 13)%>% 
+        clearPopups()
+    }
+  })
   
   
 })
-
-
 
 
 
